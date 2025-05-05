07f2a5e88602fde395fadb030e579cfe9f466462
TaravanMierlo <taravanmierlo@live.nl>
2025-05-05 21:43:39 +0200

    bug fix

--- Streamlit_How.py
+++ Streamlit_How.py
@@ -89,12 +89,14 @@ with col1:
 
 with col2:
     with st.expander("ℹ️ **Invloed per kenmerk**"):
-        st.write( "Deze grafieken tonen hoe verschillende klinische variabelen bijdragen aan het voorspelde risico op sepsis-geassocieerde delier."\
-                 "- X-as: Waarden van een specifieke variabele. " \
-                 "- Y-as: De invloed van deze waarde op de voorspelling van het model. Hoe verder een punt van nul ligt (positief of negatief), hoe groter de impact op het advies."
-                 "- Oranje punten duiden op waarden die het voorspelde risico op SAD verhogen."\
-                 "- Blauwe punten duiden op waardien die het voorspelde risico verlagen."\
-        "Deze functie is geleerd tijdens het trainen en bepaalt welk signaal een bepaalde invoerwaarde bijdraagt aan het uiteindelijke advies.")
+        st.write(
+            "Deze grafieken tonen hoe verschillende klinische variabelen bijdragen aan het voorspelde risico op sepsis-geassocieerde delier. "
+            "- X-as: Waarden van een specifieke variabele. "
+            "- Y-as: De invloed van deze waarde op de voorspelling van het model. Hoe verder een punt van nul ligt (positief of negatief), hoe groter de impact op het advies. "
+            "- Oranje punten duiden op waarden die het voorspelde risico op SAD verhogen. "
+            "- Blauwe punten duiden op waarden die het voorspelde risico verlagen. "
+            "Deze functie is geleerd tijdens het trainen en bepaalt welk signaal een bepaalde invoerwaarde bijdraagt aan het uiteindelijke advies."
+        )
     
     selected_label = st.selectbox(
         "Selecteer een kenmerk om de invloed op het advies te bekijken",
