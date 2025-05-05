96a0de95718f33ef3e2c35f8832b66859473ccab
TaravanMierlo <taravanmierlo@live.nl>
2025-05-05 13:33:45 +0200

    added local spline plots

--- Streamlit_Why.py
+++ Streamlit_Why.py
@@ -15,6 +15,12 @@ model.spline_preacts = checkpoint['model_spline_preacts']
 model.spline_postacts = checkpoint['model_spline_postacts']
 model.eval()
 
+# ========== Load Feature Info ==========
+with open("models/feature_config.pkl", "rb") as f:
+    feature_config = pickle.load(f)
+
+feature_names = feature_config["continuous_labels"] + feature_config["binary_labels"] + feature_config["ordinal_labels"]
+
 # Load data
 with open("models/original_df.pkl", "rb") as f:
     original_df = pickle.load(f)
@@ -41,4 +47,68 @@ with column2:
     st.write("ℹ️ **Ranglijst van Kenmerkbelang**")
     st.image("static/global_feature_importance_bar.png", use_container_width=True)  # Placeholder
 
+    feature_list = [
+    "Protrombinetijd (s)",
+    "INR",
+    "Temperatuur (Celcius)",
+    "Mechanische ventilatie n (%)",
+    "GCS",
+    "SOFA",
+    "ICU Type: NICU",
+    "Beroerte n (%)",
+    "Magnesium (mg/dL)",
+    "AKI n (%)",
+    "Natrium (mEq/L)",
+    "SpO2",
+    "Creatinine (mg/dL)",
+    "Sedatie n (%)",
+    "Vasopressor n (%)",
+    "CRRT n (%)",
+    "BUN (mg/dL)",
+    "Witte bloedcellen (k/uL)",
+    "Afkomst: Anders",
+    "ICU Type: CVICU",
+    "Hartslag (Slagen per Minuut)",
+    "PTT (s)",
+    "ICU Type: CCU",
+    "Ademhalingsfrequentie",
+    "COPD n (%)",
+    "ICU Type: MICU",
+    "Gewicht (Kg)",
+    "Leeftijd",
+    "Chloride (mEq/L)",
+    "ICU Type: SICU",
+    "Diastolische bloeddruk (mmHg)",
+    "Hemoglobine (g/dL)",
+    "Bicarbonaat (mEq/L)",
+    "Fosfaat (mg/dL)",
+    "Systolische bloeddruk (mmHg)",
+    "Afkomst: Aziatisch",
+    "Hypertensie n (%)",
+    "Gemiddelde arteriele druk (mmHg)",
+    "Afkomst: Onbekend",
+    "Afkomst: Afrikaans",
+    "Totale calcium (mg/dL)",
+    "Bloedplaatjes (k/uL)",
+    "Anion gap (mEq/L)",
+    "Afkomst: Latijns-amerikaans",
+    "Geslacht (Male)",
+    "Kalium (mEq/L)",
+    "ICU Type: MICU/SICU",
+    "ICU Type: TSICU",
+    "Afkomst: Europees/Westers",
+    "AMI n (%)",
+    "Diabetes n (%)",
+    "Glucose (mg/dL)",
+    "CKD n (%)"
+]
 
+    selected_label = st.selectbox(
+        "Selecteer een kenmerk om de invloed op het advies te bekijken",
+        feature_list['Protrombinetijd (s)']
+    )
+    feature_idx = feature_names.index(selected_label)
+    
+    # Show spline activation for selected feature (layer 0)
+    img_path = f"static/splines/layer0_input{feature_idx}_to_output0.png"
+    st.image(img_path, use_container_width=True)
