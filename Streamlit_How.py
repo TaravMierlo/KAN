8f2434b951886115056c332966805ba0e93d3cfe
TaravanMierlo <taravanmierlo@live.nl>
2025-05-15 18:10:00 +0200

    wider text width

--- Streamlit_How.py
+++ Streamlit_How.py
@@ -58,7 +58,7 @@ with col1:
 
     .tooltip .tooltiptext {{
         visibility: hidden;
-        width: 200px;
+        width: 600px;
         background-color: #fff;
         color: #000;
         text-align: center;
