ba423428150999d4e4e18a931996d8207e34bb80
TaravanMierlo <taravanmierlo@live.nl>
2025-05-18 23:08:27 +0200

    added text

--- Streamlit_htsbt.py
+++ Streamlit_htsbt.py
@@ -96,3 +96,16 @@ with column2:
         st.markdown("- De **paarse stippellijn** markeert de inputwaarde van deze specifieke patiënt.")
         st.markdown("- De **rode stippellijn** toont de beslissingsgrens: ligt de paarse lijn rechts hiervan, dan is het advies *Geen SAD*; links is het advies *SAD*.")
         st.markdown("- De gemarkeerde waarden op de lijnen geven de ruwe modeloutput bij de inputwaarde.")
+
+    st.markdown("#### Counterfactual Uitleg per Kenmerk")
+    st.markdown("""
+    - De huidige temperatuur is 37.06 °C. Als de temperatuur lager is dan 33.85 °C, dan verandert het advies naar SAD.
+    - De huidige glucosewaarde is 119.00 mg/dL. Als de glucosewaarde hoger is dan 624.40 mg/dL, dan verandert het advies naar SAD.
+    - Het huidige natriumgehalte is 139.00 mEq/L. Als het natriumgehalte hoger is dan 145.70 mEq/L, dan verandert het advies naar SAD.
+    - De huidige INR is 1.00. Als de INR hoger is dan 1.19, dan verandert het advies naar SAD.
+    - De patiënt krijgt momenteel geen mechanische ventilatie (0%). Als dit hoger wordt dan 37%, dan verandert het advies naar SAD.
+    - De kans op een beroerte is momenteel 0%. Als dit hoger wordt dan 65%, dan verandert het advies naar SAD.
+    - De patiënt is momenteel niet opgenomen op de NICU (0%). Als de kans op opname hoger wordt dan 77%, dan verandert het advies naar SAD.
+    - De huidige GCS-score is 15.00. Als de GCS tussen 5.14 en 13.90 ligt, dan verandert het advies naar SAD.
+    - De huidige SOFA-score is 3.00. Als de SOFA-score hoger is dan 10.06, dan verandert het advies naar SAD.
+        """)
