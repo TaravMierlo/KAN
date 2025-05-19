2f2eb0846582c8f137d8b62b4de30cb9332561cf
TaravanMierlo <taravanmierlo@live.nl>
2025-05-19 23:00:48 +0200

    remove redundant information

--- Streamlit_htsbt.py
+++ Streamlit_htsbt.py
@@ -87,16 +87,6 @@ with column2:
     img_path = f"static/cf_splines_htsbt/layer0_input{selected_idx}_to_output0.png"
     st.image(img_path, use_container_width=True)
 
-    with st.expander("ℹ️ **Uitleg van het eindadvies van het model**"):
-        st.write("Deze grafiek laat zien hoe het model tot een eindadvies komt op basis van de som van alle activatiefunctie-uitkomsten.")
-        st.markdown("- **X-as**: De som van bijdragen van alle patiëntkenmerken na verwerking door het model.")
-        st.markdown("- **Y-as**: De ruwe modeluitkomst voor beide mogelijke adviezen (SAD of Geen SAD).")
-        st.markdown("- De **blauwe lijn** toont de kanscurve voor het advies *Geen SAD*.")
-        st.markdown("- De **oranje lijn** toont de kanscurve voor het advies *SAD*.")
-        st.markdown("- De **paarse stippellijn** markeert de inputwaarde van deze specifieke patiënt.")
-        st.markdown("- De **rode stippellijn** toont de beslissingsgrens: ligt de paarse lijn rechts hiervan, dan is het advies *Geen SAD*; links is het advies *SAD*.")
-        st.markdown("- De gemarkeerde waarden op de lijnen geven de ruwe modeloutput bij de inputwaarde.")
-
     st.markdown("#### Counterfactual Uitleg per Kenmerk")
     st.markdown("""
     - De temperatuur mag niet lager worden dan 33.85 °C om hetzelfde advies te behouden.
